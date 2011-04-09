28a4bdc8cc9b982a9cd87e454a10dcc5690d9a1c
Max Goedjen <max@clinkapps.com>
2011-04-08 23:38:39 -0500

    Renaming RootViewController to TouchpadViewController.

---
+++ Remote/Troy/TouchpadViewController.h
@@ -0,0 +1,16 @@
+//
+//  TouchpadViewController.h
+//  Troy
+//
+//  Created by Max Goedjen on 4/8/11.
+//  UHD
+//
+
+#import <UIKit/UIKit.h>
+#import "TouchpadView.h"
+
+@interface TouchpadViewController : UIViewController <TouchpadDelegate> {
+    
+}
+
+@end
