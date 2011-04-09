0d3ba19d1d4463ed7839ff2ff303166c3b682ce2
Max Goedjen <max@clinkapps.com>
2011-04-08 19:09:41 -0500

    Adding TouchpadView (class to interpret user touch commands).

---
+++ Remote/Troy/TouchpadView.h
@@ -0,0 +1,16 @@
+//
+//  TouchpadView.h
+//  Troy
+//
+//  Created by Max Goedjen on 4/8/11.
+//  Copyright 2011 Clink Apps. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface TouchpadView : UIImageView {
+    
+}
+
+@end
---
+++ Remote/Troy/TouchpadView.m
@@ -0,0 +1,29 @@
+//
+//  TouchpadView.m
+//  Troy
+//
+//  Created by Max Goedjen on 4/8/11.
+//  Copyright 2011 Clink Apps. All rights reserved.
+//
+
+#import "TouchpadView.h"
+
+
+@implementation TouchpadView
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+
+@end
