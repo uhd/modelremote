b94c79922b3417328a8cc32e684668d3ea88a34e
Max Goedjen <max@clinkapps.com>
2011-04-08 20:03:45 -0500

    Added touch interpretation to TouchpadView.

--- Remote/Troy/TouchpadView.m
+++ Remote/Troy/TouchpadView.m
@@ -11,19 +11,58 @@
 
 @implementation TouchpadView
 
+@synthesize delegate;
+
 - (id)init
 {
     self = [super init];
     if (self) {
         // Initialization code here.
+        self.userInteractionEnabled = YES;
     }
     
     return self;
 }
 
+- (BOOL)canBecomeFirstResponder {
+	return YES;
+}
+
 - (void)dealloc
 {
     [super dealloc];
+}
+
+- (void)configure {
+
+    self.backgroundColor = [UIColor grayColor];
+    self.userInteractionEnabled = YES;
+
+}
+
+#pragma mark touch interpretation
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
+    
+	CGPoint point = [[touches anyObject] locationInView:nil];
+    [delegate touchpadDidMoveToPoint:point];
+    
+}
+
+- (void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event {
+	
+	CGPoint point = [[touches anyObject] locationInView:nil];
+    [delegate touchpadDidMoveToPoint:point];
+	
+}
+
+- (void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event {
+	
+	//[delegate didEndOnPoint:[[touches anyObject] locationInView:self.superview] sendingItem:self];
+	
+}
+
+- (void)touchesCancelled:(NSSet *)touches withEvent:(UIEvent *)event {
+		
 }
 
 @end
