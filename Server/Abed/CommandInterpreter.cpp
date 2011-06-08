014a944409531132abd2e9cce9e7a33d01e1d430
Brian Holtkamp <brian@Brians-MacBook-Pro.local>
2011-06-08 15:18:01 -0500

    Switched to just clicking before attempting dragging.

--- Server/Abed/CommandInterpreter.cpp
+++ Server/Abed/CommandInterpreter.cpp
@@ -60,13 +60,15 @@ void CommandInterpreter::click(TACommand command)
 	XEvent event;
 	XQueryPointer(display, RootWindow(display, DefaultScreen(display)), &event.xbutton.root, &event.xbutton.window, &event.xbutton.x_root, &event.xbutton.y_root, &event.xbutton.x, &event.xbutton.y, &event.xbutton.state);
 	XTestFakeButtonEvent(display, XK_Pointer_Button1, True, CurrentTime);
+	XTestFakeButtonEvent(display, XK_Pointer_Button1, False, CurrentTime);
+
 }
 
 void CommandInterpreter::releaseMouse(TACommand command)
 {
-	XEvent event;
+	/*XEvent event;
 	XQueryPointer(display, RootWindow(display, DefaultScreen(display)), &event.xbutton.root, &event.xbutton.window, &event.xbutton.x_root, &event.xbutton.y_root, &event.xbutton.x, &event.xbutton.y, &event.xbutton.state);
-	XTestFakeButtonEvent(display, XK_Pointer_Button1, False, CurrentTime);
+	XTestFakeButtonEvent(display, XK_Pointer_Button1, False, CurrentTime);*/
 }
 
 void CommandInterpreter::moveMouse(TACommand command)
